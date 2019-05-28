3ece94d11296ef72959385c342ffe489dac4658a
118210854 VICTOR PAIVA DOS SANTOS <victorps@lcc3-035.lcc.ufcg.edu.br>
2019-05-28 09:41:38 -0300

    coloquei metodos na facade

--- src/eco/Facade.java
+++ src/eco/Facade.java
@@ -1,5 +1,31 @@
 package eco;
 
 public class Facade {
+	
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses) {
+		
+	}
+	
+	public void cadastrarPessoa(String nome, String dni, String estado, String interesses, String partido) {
+		
+	}
+	
+	public void cadastrarDeputado(String DNI, String dataDeInicio) {
+		
+	}
+	
+	public void cadastrarPartido(String partido) {
+		
+	}
+	
+	public String exibirPessoa(String DNI) {
+		return "";
+	}
+	
+	public String exibirBase() {
+		return "";
+	}
+	
+	
 
 }
