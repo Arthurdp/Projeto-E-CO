6c999bfa7ec359e2f5dbb6045ffc2106c87bcd23
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2019-06-02 17:08:24 -0300

    documentei as classes partido e pessoa

--- src/eco/Partido.java
+++ src/eco/Partido.java
@@ -1,8 +1,16 @@
 package eco;
-
+/**
+ * Representação de um partido.
+ */
 public class Partido implements Comparable<Partido> {
+	/**
+	 * nome do partido.
+	 */
 	private String nome;
-	
+	/**
+	 * constroi um novo partido.
+	 * @param nome nome do novo partido.
+	 */
 	public Partido(String nome) {
 		this.nome = nome;
 	}
@@ -10,12 +18,16 @@ public class Partido implements Comparable<Partido> {
 	public String getNome() {
 		return this.nome;
 	}
-	
+	/**
+	 * mostra a representação de um partido no formato string.
+	 */
 	@Override
 	public String toString() {
 		return nome;
 	}
-	
+	/**
+	 * permite a comparação de dois partidos.
+	 */
 	@Override
 	public int compareTo(Partido p) {
 		return nome.compareTo(p.getNome());
--- src/eco/Pessoa.java
+++ src/eco/Pessoa.java
@@ -1,14 +1,41 @@
 package eco;
-
+/**
+ * Representação de uma pessoa.
+ *
+ */
 public class Pessoa {
-
+	/**
+	 * nome da pessoa.
+	 */
 	protected String nome;
+	/**
+	 * numero de identificação da da pessoa.
+	 */
 	protected String dni;
+	/**
+	 * estado de origem da pessoa.
+	 */
 	protected String estado;
+	/**
+	 * array de interesses da pessoa
+	 */
 	protected String[] interesses;
+	/**
+	 * partido ao qual a pessoa é filiada.
+	 */
 	protected Partido partido;
+	/**
+	 * função exercida pela pessoa.
+	 */
 	private Funcao funcao;
 	
+	/**
+	 * Constroi uma nova pessoa que não esteja filiada a nenhum partido.
+	 * @param nome nome da nova pessoa.
+	 * @param dni numero de identificação da nova pessoa.
+	 * @param estado estado de origem da nova pessoa.
+	 * @param interresses interesses da nova pessoa.
+	 */
 	public Pessoa(String nome, String dni, String estado, String interresses) {
 		validaEntrada(nome, "Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
 		validaEntrada(dni, "Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
@@ -20,6 +47,14 @@ public class Pessoa {
 		this.interesses = interresses.split(",");		
 	}
 	
+	/**
+	 * Constroi uma nova pessoa que esteja filiada a nenhum partido.
+	 * @param nome nome da nova pessoa.
+	 * @param dni numero de identificação da nova pessoa.
+	 * @param estado estado de origem da nova pessoa.
+	 * @param interresses interesses da nova pessoa.
+	 * @param partido partido ao qual a nova pessoa é filiada.
+	 */
 	public Pessoa(String nome, String dni, String estado, String interresses, Partido partido) {
 		validaEntrada(nome, "Erro ao cadastrar pessoa: nome nao pode ser vazio ou nulo");
 		validaEntrada(dni, "Erro ao cadastrar pessoa: dni nao pode ser vazio ou nulo");
@@ -32,10 +67,17 @@ public class Pessoa {
 		this.partido = partido;
 	}
 	
+	/**
+	 * Transforma a pessoa em um deputado.
+	 * @param data data de inicio na vida publica.
+	 */
 	public void virouDeputado(String data) {
 		this.funcao = new Deputado(data);
 	}
-	
+	/**
+	 * Retorna uma string com todos os interesses da pessoa.
+	 * @return
+	 */
 	protected String exibirInteresses() {
 		String interesses = "interesses: ";
 		for (String e : this.interesses) {
@@ -44,7 +86,12 @@ public class Pessoa {
 		return interesses.substring(0, interesses.length() - 2);
 	}
 	
-	public String exibirPessoa() {
+	/**
+	 * exibe as informações de uma pessoa ou de um deputado.
+	 * @param dni identificação da pessoa ou deputado.
+	 * @return
+	 */
+	public String exibirPessoa(String dni) {
 		if (funcao == null) {
 			if(this.partido == null) {
 				if(this.interesses[0].trim().equals("")) {
@@ -62,9 +109,9 @@ public class Pessoa {
 		}
 		else {
 			if(this.interesses[0].trim().equals(""))
-				return this.nome + " - " + this.dni + " (" + this.estado + ")" + " - " + this.partido + " - " + this.funcao.getData() + " - " + this.funcao.getLeisAprovadas();									
+				return "POL: " + this.nome + " - " + this.dni + " (" + this.estado + ")" + " - " + this.partido + " - " + this.funcao.getData() + " - " + this.funcao.getLeisAprovadas();									
 			else
-				return this.nome + " - " + this.dni + " (" + this.estado + ")" + " - " + this.partido + " - " + exibirInteresses() + " - " + this.funcao.getData() + " - " + this.funcao.getLeisAprovadas();									
+				return "POL: " + this.nome + " - " + this.dni + " (" + this.estado + ")" + " - " + this.partido + " - " + exibirInteresses() + " - " + this.funcao.getData() + " - " + this.funcao.getLeisAprovadas();									
 		}
 	}
 	
@@ -90,7 +137,11 @@ public class Pessoa {
 			return false;
 		return true;
 	}
-
+	/**
+	 * verifica se a entrada é vazia ou nula.
+	 * @param parametro parametro a ser avaliado.
+	 * @param mensagem mensagem de erro.
+	 */
 	public static void validaEntrada(String parametro, String mensagem) {
 		if (parametro == null)
 			throw new NullPointerException(mensagem);
@@ -98,9 +149,15 @@ public class Pessoa {
 		if (parametro.trim().equals(""))
 			throw new IllegalArgumentException(mensagem);
 	}
-	
+	/**
+	 * verifica se o dni passado na construção de uma nova pessoa é valido.
+	 * @param dni dni a ser avaliado.
+	 * @param msg mensagem de erro.
+	 */
 	static void validaDni(String dni, String msg) {
 		if(dni.trim().length() > 11 || dni.trim().length() < 11 )
+			throw new IllegalArgumentException(msg);
+		if(!dni.split("")[9].equals("-"))
 			throw new IllegalArgumentException(msg);
 		String[] dados = dni.split("");
 		for(String digito : dados) {
@@ -114,3 +171,4 @@ public class Pessoa {
 	}
 	
 }
+
