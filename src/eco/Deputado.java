7415bf6aab5e825cad2581097c53f38b839c9311
VictorPaivaDosSantos <50140824+VictorPaivaDosSantos@users.noreply.github.com>
2019-06-02 23:07:46 -0300

    criei validador de datas

--- src/eco/Deputado.java
+++ src/eco/Deputado.java
@@ -17,6 +17,7 @@ public class Deputado implements Funcao{
 	 * @param data data do inicio da vida publica do novo deputado.
 	 */
 	public Deputado(String data) {
+		validador(data);
 		this.data = data;
 		this.leisAprovadas = 0;
 	}
@@ -31,5 +32,33 @@ public class Deputado implements Funcao{
 
 	public void setLeisAprovadas(int leisAprovadas) {
 		this.leisAprovadas = leisAprovadas;
+	}
+	
+	public void validador(String data1) {
+		if(data1.length() != 8)
+			throw new IllegalArgumentException("Erro ao cadastrar deputado: data invalida");
+		
+		String[] numeros = data1.split("");
+		for(String digito : numeros) {
+			if(!digito.equals("-")) 
+				if(!digito.matches("[0-9]"))
+					throw new IllegalArgumentException("Erro ao cadastrar deputado: data invalida");
+		}
+		
+		if(data1.trim().equals(""))
+			throw new NullPointerException("Erro ao cadastrar deputado: data nao pode ser vazio ou nulo");
+		
+		
+		Date hoje = new Date();
+		Date data2 = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+		try {
+			data2 = sdf.parse(data1);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(data2.after(hoje))
+			throw new IllegalArgumentException("Erro ao cadastrar deputado: data futura");
 	}
 }
