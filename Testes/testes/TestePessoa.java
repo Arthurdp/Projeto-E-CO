45d95d91c97aae720db8c511443193b1e2088574
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2019-06-03 20:54:01 -0300

    olha ai vitor

--- Testes/testes/TestePessoa.java
+++ Testes/testes/TestePessoa.java
@@ -14,14 +14,14 @@ class TestePessoa {
 	
 	@BeforeEach
 	void criarPessoas() {
-		pessoa1 = new Pessoa("Robervaldo", "12345678911", "RJ", "trafico,caixa2,sonegação,lavagem de dinheiro");
-		pessoa2 = new Pessoa("Tiririca", "21345678911", "CE", "abestado,fazer menino,comer farinha", "PCB");
+		pessoa1 = new Pessoa("Robervaldo", "123456789-1", "RJ", "trafico,caixa2,sonegação,lavagem de dinheiro");
+		pessoa2 = new Pessoa("Tiririca", "213456789-1", "CE", "abestado,fazer menino,comer farinha", "PCB");
 	}
 	
 	@Test
 	void TesteConstrutor() {
 		try {
-			Pessoa pessoa = new Pessoa(null, "22222222222","AL", "fdsfsdf,safsasa,dadas");
+			Pessoa pessoa = new Pessoa(null, "222222222-2","AL", "fdsfsdf,safsasa,dadas");
 			fail("Deveria lançar excessão!");
 		}catch(NullPointerException e) {}
 		
@@ -31,17 +31,17 @@ class TestePessoa {
 		}catch(NullPointerException e) {}
 		
 		try {
-			Pessoa pessoa = new Pessoa("AL", "22222222222", null, "fdsfsdf,safsasa,dadas");
+			Pessoa pessoa = new Pessoa("AL", "222222222-2", null, "fdsfsdf,safsasa,dadas");
 			fail("Deveria lançar excessão!");
 		}catch(NullPointerException e) {}
 		
 		try {
-			Pessoa pessoa = new Pessoa("AL", "22222222222", "fdsfsdf,safsasa,dadas", null);
+			Pessoa pessoa = new Pessoa("AL", "222222222-2", "fdsfsdf,safsasa,dadas", null);
 			fail("Deveria lançar excessão!");
 		}catch(NullPointerException e) {}
 		
 		try {
-			Pessoa pessoa = new Pessoa("", "11111111111", "PB", "mnbdbf,fsdfsdf,fdsfsdf");
+			Pessoa pessoa = new Pessoa("", "111111111-1", "PB", "mnbdbf,fsdfsdf,fdsfsdf");
 			fail("Deveria lançar excessão!");
 		}catch(IllegalArgumentException e) {}
 		
@@ -51,15 +51,15 @@ class TestePessoa {
 		}catch(IllegalArgumentException e) {}
 		
 		try {
-			Pessoa pessoa = new Pessoa("hdjsh", "33333333333", "", "kjghdkjgxdk,sddxvxf,vfcvcf");
+			Pessoa pessoa = new Pessoa("hdjsh", "333333333-3", "", "kjghdkjgxdk,sddxvxf,vfcvcf");
 			fail("Deveria lançar excessão!");
 		}catch(IllegalArgumentException e) {}
 	}
 	
 	@Test
 	void testeExibirPessoa() {
-		assertEquals("Robervaldo - 12345678911 (RJ) - interesses: trafico, caixa2, sonegação, lavagem de dinheiro", pessoa1.exibirPessoa());
-		assertEquals("Tiririca - 21345678911 (CE) - PCB - interesses: abestado, fazer menino, comer farinha", pessoa2.exibirPessoa());
+		assertEquals("Robervaldo - 123456789-1 (RJ) - interesses: trafico,caixa2,sonegação,lavagem de dinheiro", pessoa1.exibirPessoa());
+		assertEquals("Tiririca - 213456789-1 (CE) - PCB - interesses: abestado, fazer menino, comer farinha", pessoa2.exibirPessoa());
 		pessoa2.virouDeputado("11112016");
 	}
 
