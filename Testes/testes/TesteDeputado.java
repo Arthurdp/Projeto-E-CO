e24da0d95c3229c646f18c451aba950927457039
VictorPaivaDosSantos <50140824+VictorPaivaDosSantos@users.noreply.github.com>
2019-06-03 21:40:31 -0300

    criei testes de deputado

---
+++ Testes/testes/TesteDeputado.java
@@ -0,0 +1,47 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import eco.Pessoa;
+
+class TesteDeputado {
+	
+	Pessoa pessoa1;
+	Pessoa pessoa2;
+
+	@BeforeEach
+	void criarPessoas() {
+		pessoa1 = new Pessoa("Robervaldo", "123456781-1", "RJ", "trafico,caixa2,sonegação,lavagem de dinheiro");
+		pessoa2 = new Pessoa("Tiririca", "213456781-1", "CE", "abestado,fazer menino,comer farinha", "PCB");
+	}
+
+	@Test
+	void testDeputado() {
+		pessoa2.virouDeputado("02062019");
+		assertEquals("POL: Tiririca - 213456781-1 (CE) - PCB - interesses: abestado, fazer menino, comer farinha - 02062019 - 0", pessoa2.exibirPessoa());
+		
+		try {
+			pessoa2.virouDeputado(null);
+			fail("Deveria lançar excessão!");
+		}catch(NullPointerException e) {}
+		
+		try {
+			pessoa2.virouDeputado("");
+			fail("Deveria lançar excessão!");
+		}catch(IllegalArgumentException e) {}
+		
+		try {
+			pessoa2.virouDeputado("20072019");
+			fail("Deveria lançar excessão!");
+		}catch(IllegalArgumentException e) {}
+		
+		try {
+			pessoa2.virouDeputado("33072019");
+			fail("Deveria lançar excessão!");
+		}catch(IllegalArgumentException e) {}
+	}
+
+}
