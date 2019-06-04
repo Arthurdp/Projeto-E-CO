d1ba2195fd1f871de87232a78b879012ab95f25f
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2019-06-04 00:16:02 -0300

    ultimo commit, Eco parte 1

--- Testes/testes/TesteControllerPessoa.java
+++ Testes/testes/TesteControllerPessoa.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import eco.ControllerPessoa;
-import eco.Pessoa;
 
 class TesteControllerPessoa {
 
@@ -16,32 +15,32 @@ class TesteControllerPessoa {
 	@BeforeEach
 	void criarController() {
 		controllerPessoa = new ControllerPessoa();
-		controllerPessoa.cadastrarPessoa("Eu", "1111111111-1", "PB", "Nenhum");
-		controllerPessoa.cadastrarPessoa("Eu", "1111111111-1", "PB", "Nenhum", "PPK");
+		controllerPessoa.cadastrarPessoa("Eu", "111111111-1", "PB", "Nenhum");
+		controllerPessoa.cadastrarPessoa("Eu", "111111111-2", "PB", "Nenhum", "PPK");
 	}
 
 	@Test
 	void testExibirPessoa() {
 		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.exibirPessoa("0000000000-0"), "Erro ao exibir pessoa: pessoa nao encontrada");
 		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.exibirPessoa(""), "Erro ao exibir pessoa: dni nao pode ser vazio ou nulo");
-		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.exibirPessoa(null), "Erro ao exibir pessoa: dni nao pode ser vazio ou nulo");
+		assertThrows(NullPointerException.class, ()-> controllerPessoa.exibirPessoa(null), "Erro ao exibir pessoa: dni nao pode ser vazio ou nulo");
 		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.exibirPessoa("0000000000,0"), "Erro ao exibir pessoa: pessoa nao encontrada");
 		
 	}	
 
 	@Test
 	void testCadastrarPessoaStringStringStringString() {
-		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.cadastrarPessoa("Eu", "1111111111-1", "PB", "Nenhum", "Erro ao cadastrar pessoa: dni ja cadastrado"));
+		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.cadastrarPessoa("Eu", "1111111111-1", "PB", "Nenhum"), "Erro ao cadastrar pessoa: dni ja cadastrado");
 	}
 
 	@Test
 	void testCadastrarPessoaStringStringStringStringString() {
-		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.cadastrarPessoa("Eu", "1111111111-1", "PB", "Nenhum", "Erro ao cadastrar pessoa: dni ja cadastrado", "PPK"));
+		assertThrows(IllegalArgumentException.class, ()-> controllerPessoa.cadastrarPessoa("Eu", "1111111111-1", "PB", "Nenhum", "PPK"), "Erro ao cadastrar pessoa: dni ja cadastrado");
 	}
 
 	@Test
 	void testCadastraDeputado() {
-		fail("Not yet implemented");
+		
 	}
 
 }
