550afe0bedde45eaeaa8012d733856d5cb7d6bf2
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2019-06-12 22:49:39 -0300

    botei as exceções da us6

--- src/eco/Controller.java
+++ src/eco/Controller.java
@@ -122,16 +122,57 @@ public class Controller {
 	}
 	
 	public void cadastrarPL(String dni, int ano, String ementa, String interesses, String url, boolean conclusivo) {
+		validador.validaEntrada(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
+		validador.validaEntrada(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
+		validador.validaEntrada(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
+		validador.validaEntrada(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
+		validador.validaDni(dni, "Erro ao cadastrar projeto: dni invalido");
+		if(!controllerPessoa.contem(dni)) 
+			throw new NullPointerException("Erro ao cadastrar projeto: pessoa inexistente");
+		if(!controllerPessoa.eDeputado(dni))
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: pessoa nao eh deputado");
+		if(ano < 1988)
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: ano anterior a 1988");
+		if(ano > 2019)
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: ano posterior ao ano atual");
 		this.projetosDeLei.put("PL " + this.contadorPL + "/" + ano, new PL(dni, ano, this.contadorPL, ementa, interesses, url, conclusivo));
 		this.contadorPL++;
 	}
 	
 	public void cadastrarPLP(String dni, int ano, String ementa, String interesses, String url, String artigos) {
+		validador.validaEntrada(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
+		validador.validaEntrada(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
+		validador.validaEntrada(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
+		validador.validaEntrada(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
+		validador.validaEntrada(artigos, "Erro ao cadastrar projeto: artigo nao pode ser vazio ou nulo");
+		validador.validaDni(dni, "Erro ao cadastrar projeto: dni invalido");
+		if(!controllerPessoa.contem(dni)) 
+			throw new NullPointerException("Erro ao cadastrar projeto: pessoa inexistente");
+		if(!controllerPessoa.eDeputado(dni))
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: pessoa nao eh deputado");
+		if(ano < 1988)
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: ano anterior a 1988");
+		if(ano > 2019)
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: ano posterior ao ano atual");
 		this.projetosDeLei.put("PLP " + this.contadorPLP + "/" + ano, new PLP(dni, ano, this.contadorPLP, ementa, interesses, url, artigos));
 		this.contadorPLP++;
 	}
 	
 	public void cadastrarPEC(String dni, int ano, String ementa, String interesses, String url, String artigos) {
+		validador.validaEntrada(dni, "Erro ao cadastrar projeto: autor nao pode ser vazio ou nulo");
+		validador.validaEntrada(ementa, "Erro ao cadastrar projeto: ementa nao pode ser vazia ou nula");
+		validador.validaEntrada(interesses, "Erro ao cadastrar projeto: interesse nao pode ser vazio ou nulo");
+		validador.validaEntrada(url, "Erro ao cadastrar projeto: url nao pode ser vazio ou nulo");
+		validador.validaEntrada(artigos, "Erro ao cadastrar projeto: artigo nao pode ser vazio ou nulo");
+		validador.validaDni(dni, "Erro ao cadastrar projeto: dni invalido");
+		if(!controllerPessoa.contem(dni)) 
+			throw new NullPointerException("Erro ao cadastrar projeto: pessoa inexistente");
+		if(!controllerPessoa.eDeputado(dni))
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: pessoa nao eh deputado");
+		if(ano < 1988)
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: ano anterior a 1988");
+		if(ano > 2019)
+			throw new IllegalArgumentException("Erro ao cadastrar projeto: ano posterior ao ano atual");
 		this.projetosDeLei.put("PEC " + this.contadorPEC + "/" + ano, new PEC(dni, ano, this.contadorPEC, ementa, interesses, url, artigos));
 		this.contadorPEC++;
 	}
