64f4254de6cd1123ecb99a228ff3b0d10e0cfe51
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2019-06-25 17:35:44 -0300

    terminados os ordenadores

--- src/eco/ordenaPelaConclusao.java
+++ src/eco/ordenaPelaConclusao.java
@@ -16,7 +16,37 @@ public class ordenaPelaConclusao implements Comparator<Projeto>{
 		else if(!projeto1.getLocalAtual().equals("plenario") && projeto2.getLocalAtual().equals("plenario"))
 			return 1;
 		
-		else
+		else if(projeto1.getAprovacoes() - projeto2.getAprovacoes() != 0)
 			return projeto1.getConclusoes() - projeto2.getConclusoes();
+		
+		else {
+			
+			if(projeto1.getAno() < projeto2.getAno()) {
+				return -1;
+			}
+			
+			else if(projeto1.getAno() > projeto2.getAno()) {
+				return 1;
+			}
+			
+			else {
+				 String[] array = projeto1.getCodigo().split(" ");
+				 String[] array2 = projeto2.getCodigo().split(" ");
+				 char numero = array[1].charAt(0);
+				 char numero2 = array2[1].charAt(0);
+				 
+				 if(numero < numero2) {
+					 return -1;
+				 }
+				 
+				 else if(numero > numero2) {
+					 return 1;
+				 }
+				 
+				 else
+					 throw new IllegalArgumentException("Nao era para estar lancando esse erro, fale com o tecnico a respeito desse bug");
+		
+			}
+		}
 	}
 }
