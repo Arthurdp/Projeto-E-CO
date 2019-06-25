1b23ff3c17d808afe6e6b27dcaa2ef8d1a06991e
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2019-06-25 17:17:59 -0300

    ...

--- src/eco/ordenaPelaConclusao.java
+++ src/eco/ordenaPelaConclusao.java
@@ -1,5 +1,22 @@
 package eco;
 
-public class ordenaPelaConclusao {
+import java.util.Comparator;
 
+public class ordenaPelaConclusao implements Comparator<Projeto>{
+	public int compare(Projeto projeto1, Projeto projeto2) {
+		if(projeto1.getSituacaoAtual().equals("EM VOTACAO (Plenario - 2o turno)") && !projeto2.getSituacaoAtual().equals("EM VOTACAO (Plenario - 2o turno)"))
+			return -1;
+		
+		else if(!projeto1.getSituacaoAtual().equals("EM VOTACAO (Plenario - 2o turno)") && projeto2.getSituacaoAtual().equals("EM VOTACAO (Plenario - 2o turno)"))
+			return 1;
+		
+		else if(projeto1.getLocalAtual().equals("plenario") && !projeto2.getLocalAtual().equals("plenario"))
+			return -1;
+		
+		else if(!projeto1.getLocalAtual().equals("plenario") && projeto2.getLocalAtual().equals("plenario"))
+			return 1;
+		
+		else
+			return projeto1.getConclusoes() - projeto2.getConclusoes();
+	}
 }
