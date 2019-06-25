fb2d3c5bfd7c2c2240ac2365976c33311b45689d
118210278 WILLY GUIMARAES MORAIS BARROS <willy.barros@ccc.ufcg.edu.br>
2019-06-25 10:28:50 -0300

    testando

--- src/eco/PEC.java
+++ src/eco/PEC.java
@@ -25,7 +25,7 @@ public class PEC extends ProjetosConstitucionais{
 
 public boolean votarPlenario(String estatusGovernista, List<Pessoa> politicos, List<Pessoa> politicosPresentes, List<String> partidos) {
 		
-		if(politicosPresentes.size() <= Math.floor((3/5 * politicos.size())) + 1)
+		if(politicosPresentes.size() <= Math.floor((3/5 * politicos.size())) + 1) 
 			throw new IllegalArgumentException("Erro ao votar proposta: quorum invalido");
 		
 		if(getSituacaoAtual().equals("APROVADO") || getSituacaoAtual().equals("ARQUIVADO"))
