ca0fb5f3f5542f34b01740e921c3e4c4df665f29
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2019-06-26 13:22:15 -0300

    simplificadas as classes de selecao da us9

--- src/eco/Constitucional.java
+++ src/eco/Constitucional.java
@@ -5,34 +5,6 @@ import java.util.List;
 
 public class Constitucional implements Estrategia {
 	
-	public Projeto oMaisVelho(List<Projeto> projetos) {
-	   	Projeto maisVelho = projetos.get(0);
-		int i = 0;
-	   	
-		for(int j = i + 1; j < projetos.size(); j++) {
-	   		if(i == projetos.size() - 1)
-	   			break;
-
-	   		if(projetos.get(i).getAno() > projetos.get(j).getAno()) {
-	   			maisVelho = projetos.get(j);
-			   	i = j;
-			}
-			   	 
-	   		else if(projetos.get(i).getAno() == projetos.get(j).getAno()) {
-				 String[] array = projetos.get(i).getCodigo().split(" ");
-				 String[] array2 = projetos.get(j).getCodigo().split(" ");
-				 int numero = Integer.parseInt(array[1].split("")[0]);
-				 int numero2 = Integer.parseInt(array2[1].split("")[0]);
-			   		 
-				 if(numero < numero2) {
-					 i = j;
-					 maisVelho = projetos.get(j);
-		   		 }
-		   	 }
-	   	}
-		return maisVelho;
-    }
-	
 public String prioridade(List<Projeto> projetos) {
 		
 		if(projetos.size() == 0)
--- src/eco/PorAprovacao.java
+++ src/eco/PorAprovacao.java
@@ -4,33 +4,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class PorAprovacao implements Estrategia{
-	public Projeto oMaisVelho(List<Projeto> projetos) {
-	   	Projeto maisVelho = projetos.get(0);
-		int i = 0;
-	   	
-		for(int j = i + 1; j < projetos.size(); j++) {
-	   		if(i == projetos.size() - 1)
-	   			break;
-
-	   		if(projetos.get(i).getAno() > projetos.get(j).getAno()) {
-	   			maisVelho = projetos.get(j);
-			   	i = j;
-			}
-			   	 
-	   		else if(projetos.get(i).getAno() == projetos.get(j).getAno()) {
-				 String[] array = projetos.get(i).getCodigo().split(" ");
-				 String[] array2 = projetos.get(j).getCodigo().split(" ");
-				 char numero = array[1].charAt(0);
-				 char numero2 = array2[1].charAt(0);
-			   		 
-				 if(numero > numero2) {
-					 maisVelho = projetos.get(j);
-					 i = j;
-		   		 }
-		   	 }
-	   	}
-		return maisVelho;
-    }
 	
 	public String prioridade(List<Projeto> projetos) {
 		
