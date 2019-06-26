6503a4c869d9d0217009dfc9d227234a86336724
Renan Nunes Viana <renan.viana@ccc.ufcg.edu.br>
2019-06-26 19:11:44 -0300

    corrigido os erros do comparaConclusao

--- src/eco/ComparaConclusao.java
+++ src/eco/ComparaConclusao.java
@@ -4,12 +4,11 @@ import java.util.Comparator;
 
 public class ComparaConclusao implements Comparator<Projeto>{
 	public int compare(Projeto projeto1, Projeto projeto2) {
-		if(projeto1.getConclusoes() - projeto2.getConclusoes() != 0) {
-			if(projeto1.getConclusoes() < projeto2.getConclusoes())
+		if(projeto1.getConclusoes() < projeto2.getConclusoes())
 				return 1;
-		}else if(projeto1.getConclusoes() > projeto2.getConclusoes()) {
+		else if(projeto1.getConclusoes() > projeto2.getConclusoes())
 			return -1;
-		}
+		
 				
 			
 		else {
@@ -22,7 +21,10 @@ public class ComparaConclusao implements Comparator<Projeto>{
 				 char numero = array[1].charAt(0);
 				 char numero2 = array2[1].charAt(0);
 				 
-				 return numero - numero2;
+				 if (numero < numero2)
+					 return -1;
+				 else if (numero > numero2)
+					 return 1;
 			}
 				
 		}
